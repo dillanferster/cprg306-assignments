48471a84bd2a66f1776018d728bf6eb34c5de03d
Dillan <dillyferster@gmail.com>
2024-01-16 13:21:50 -0700

    week2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1 className="">Dillan</h1>
+      <Link href="https://github.com/dillanferster" target="blank">
+        GitHub
+      </Link>
+    </main>
+  );
+}
